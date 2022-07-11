a0e2ceaeba004c337655dadea599a9c89fe4d292
Lesslie Houck <Lesslie00@outlook.com>
2022-07-10 20:21:05 -0700

    Update to Click Event Code in Script

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ h1.textContent = "art, life, home"
 
 document.querySelector("body").appendChild(h2) 
 
+const sinon = require(sinon)
+
 function addingEventListener() {
     const input = document.getElementById('button');
     input.addEventListener('click', function() {
