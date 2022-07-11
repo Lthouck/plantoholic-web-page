9267ca09c0dad04468e8d399461d1a91f1fcccfd
Lesslie Houck <Lesslie00@outlook.com>
2022-07-10 20:12:06 -0700

    Added Click Event in HTML/Code in Script

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const h2 = document.createElement("h2")
 h1.textContent = "art, life, home"
 
 document.querySelector("body").appendChild(h2) 
+
+function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+      alert('You signed up!')
+    })
+}
+addingEventListener() 
