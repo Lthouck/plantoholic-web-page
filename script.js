0091716faefa334d02e71faa6222ae6bfe27447a
Lesslie Houck <Lesslie00@outlook.com>
2022-07-10 18:01:15 -0700

    Updated HTML Sub-Heads/Images

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const h2 = document.createElement("h2")
-h2.textContent = "hartful botanicals"
+const h1 = document.createElement("h2")
+h1.textContent = "hartful botanicals"
 
-document.querySelector("body").appendChild(h2) 
+document.querySelector("body").appendChild(h1) 
